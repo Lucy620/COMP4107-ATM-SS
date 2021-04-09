8299aa969a16af99fdf43e464a7532b3538e8f24
lusi <lusi620@163.com>
2021-04-09 18:27:10 +0800

    Push update

--- src/ATMSS/ATMSS/ATMSS.java
+++ src/ATMSS/ATMSS/ATMSS.java
@@ -51,10 +51,47 @@ public class ATMSS extends AppThread {
         super(id, appKickstarter);
         pollingTime = Integer.parseInt(appKickstarter.getProperty("ATMSS.PollingTime"));
         String urlPrefix = "http://cslinux0.comp.hkbu.edu.hk/comp4107_20-21_grp06/";
-        bams = new BAMSHandler(urlPrefix);
+        bams = new BAMSHandler(urlPrefix,initLogger());
     } // ATMSS
 
+    static Logger initLogger() {
+        // init our logger
+        ConsoleHandler logConHdr = new ConsoleHandler();
+        logConHdr.setFormatter(new LogFormatter());
+        Logger log = Logger.getLogger("TestBAMSHandler");
+        log.setUseParentHandlers(false);
+        log.setLevel(Level.ALL);
+        log.addHandler(logConHdr);
+        logConHdr.setLevel(Level.ALL);
+        return log;
+    } // initLogger
 
+
+    static class LogFormatter extends Formatter {
+        //------------------------------------------------------------
+        // format
+        public String format(LogRecord rec) {
+            Calendar cal = Calendar.getInstance();
+            String str = "";
+
+            // get date
+            cal.setTimeInMillis(rec.getMillis());
+            str += String.format("%02d%02d%02d-%02d:%02d:%02d ",
+                    cal.get(Calendar.YEAR) - 2000,
+                    cal.get(Calendar.MONTH) + 1,
+                    cal.get(Calendar.DAY_OF_MONTH),
+                    cal.get(Calendar.HOUR_OF_DAY),
+                    cal.get(Calendar.MINUTE),
+                    cal.get(Calendar.SECOND));
+
+            // level of the log
+            str += "[" + rec.getLevel() + "] -- ";
+
+            // message of the log
+            str += rec.getMessage();
+            return str + "\n";
+        } // format
+    } // LogFormatter
     //------------------------------------------------------------
     // run
     public void run() {
